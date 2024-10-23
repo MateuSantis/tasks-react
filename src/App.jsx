72a880bf99d2fafe312816479aa29a01ffd9473d
Mateus Santis <mateusantis2@gmail.com>
2024-10-23 17:36:00 -0300

    subindo lista

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor Full Stack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Ingles",
+      description:
+        "Estudar ingles para se tornar fluente",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Matematica",
+      description:
+        "Estudar matematica para se tornar um programador full stack",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks />
+        <AddTask />
+      </div>
+    </div>
+  );
+}
+
+export default App;
